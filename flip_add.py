4dfc4612c6ed55e7722ea19bcb7e11ee61b0d87b
julschoen <julianschoen@gmx.de>
2023-08-15 09:36:44 +0200

    Update flip_add.py

--- flip_add.py
+++ flip_add.py
@@ -40,7 +40,7 @@ def train_in_painter(settings):
     if file_ids is None:
         return
 
-    inpainted_result_dir = os.path.join(output_data_dir, f"inpainted_{data_set}")
+    inpainted_result_dir = os.path.join(output_data_dir, f"inpainted_{settings["data_set"]}")
     pathlib.Path(inpainted_result_dir).mkdir(parents=True, exist_ok=True)
 
     print(f"Training on {len(file_ids)} images")
