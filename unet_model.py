68cefed59acf75ef96d26035cec0b10917aaec2a
julschoen <julianschoen@gmx.de>
2023-08-15 18:01:46 +0200

    Fix Unet

--- unet_model.py
+++ unet_model.py
@@ -4,7 +4,7 @@ from unet_parts import *
 
 
 class UNet(nn.Module):
-    def __init__(self, n_channels, n_classes, bilinear=False):
+    def __init__(self, n_channels=3, bilinear=False):
         super(UNet, self).__init__()
         self.n_channels = n_channels
         self.n_classes = n_classes
@@ -20,7 +20,7 @@ class UNet(nn.Module):
         self.up2 = (Up(512, 256 // factor, bilinear))
         self.up3 = (Up(256, 128 // factor, bilinear))
         self.up4 = (Up(128, 64, bilinear))
-        self.outc = (OutConv(64, n_classes))
+        self.outc = (OutConv(64, n_channels))
 
     def forward(self, x):
         x1 = self.inc(x)
