4bd2bd06c351d4cb182fa158f396ad9bc1fb4be9
julschoen <julianschoen@gmx.de>
2023-08-16 11:50:19 +0200

    Update unet_trainer.py

--- unet_trainer.py
+++ unet_trainer.py
@@ -173,6 +173,7 @@ class Trainer(object):
             loss.backward()
             self.opt.step()
 
+            if i%10==0: print(loss)
             self.losses.append((mse_loss.detach().item(), ssim_loss.detach().item()))
             self.log(i, rec, im)
             if i%100 == 0 and i>0:
